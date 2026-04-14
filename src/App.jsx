5bab383847f5adf4396e1a31dfb1c2183e8d9baa
ilhamaji <iheadskull@gmail.com>
2026-04-14 14:02:12 +0700

    feat: migrate to BrowserRouter for clean URLs (no hash)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
@@ -7,7 +7,7 @@ import About from "./pages/About";
 function App() {
   return (
     <ThemeProvider>
-      <Router>
+      <Router basename={import.meta.env.VITE_BASE_PATH || "/"}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/details/:name" element={<Details />} />
